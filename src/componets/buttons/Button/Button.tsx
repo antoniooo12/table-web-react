58af347595298dd819bfc3a78ab57b0cb3d9642e
AntonOmelianenko <anton.omelianenko@dev.pro>
2022-06-09 22:49:56 +0300

    improve Button component

--- src/componets/buttons/Button/Button.tsx
+++ src/componets/buttons/Button/Button.tsx
@@ -3,13 +3,15 @@ import React from 'react';
 import clsx from "clsx";
 
 export type TButton = React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> & {
-    style?: 'default' | 'blue' | 'white'
+    style?: 'default' | 'blue' | 'white' |'gray' | 'danger'
 }
 
 const Button: React.FC<TButton> = ({style = 'default', children, ...props}) => {
     const color = clsx({
         [cl.blue]: style === 'blue' || 'default',
         [cl.white]: style === 'white',
+        [cl.gray]: style === 'gray',
+        [cl.danger]: style === 'danger',
     })
     return (
         <button
